d6aa32e1aafda908e48be74e924dc219f45d324b
Yuriy <mazalov-yurka@rambler.ru>
2023-07-11 11:27:34 +0300

    111

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { createHomeMainSection } from './js/home-best-sellers';
+import './js/header';
+import './js/categories-list.js';
+import './js/support-ua';
+import './js/home-best-sellers';
+import './js/books-by-category';
+import './js/book-modal/book-modal';
+import './js/window-reload';
+import './js/mobile-menu';
+import './js/auth/auth.js';
+import './js/modal-footer-team';
+import './js/burger';
---
+++ src/js/book-modal/book-modal.js
@@ -0,0 +1,69 @@
+import { getBookId } from '../utils/get-books-id';
+import { createMarkup } from './create-modal';
+
+const book = document.querySelector('.js-home-category-books-list');
+
+const basketArr = JSON.parse(localStorage.getItem('books')) ?? [];
+
+function onClick(evt) {
+  //   console.log(evt.target);
+  const bookCard = evt.target.parentNode;
+  const bookCardId = bookCard.dataset.id;
+
+  getBookId(bookCardId)
+    .then(obj => {
+      if (bookCard.classList.contains('js-home-books-item')) {
+        createMarkup(obj);
+      }
+
+      const addBook = document.querySelector('#js-book-modal-btn');
+
+      document.querySelector('.book-modal-buy').style.display = 'none';
+      // addBook.addEventListener('click', addToCart);
+
+      const inStorage = basketArr.some(({ _id }) => _id === obj._id);
+      if (inStorage) {
+        addBook.addEventListener('click', addToCart);
+
+        addBook.classList.remove('js-add');
+        addBook.classList.add('js-remove');
+        document.querySelector('.book-modal-buy').style.display = 'block';
+      } else {
+        addBook.addEventListener('click', addToCart);
+
+        addBook.classList.add('js-add');
+        addBook.classList.remove('js-remove');
+        document.querySelector('.book-modal-buy').style.display = 'none';
+      }
+
+      function addToCart(evt) {
+        const btnAdd = evt.target.classList.contains('js-add');
+        const inStorage = basketArr.some(({ _id }) => _id === obj._id);
+
+        if (btnAdd) {
+          evt.target.classList.remove('js-add');
+          evt.target.classList.add('js-remove');
+          document.querySelector('.book-modal-buy').style.display = 'block';
+
+          if (inStorage) {
+            return;
+          } else {
+            basketArr.push(obj);
+            localStorage.setItem('books', JSON.stringify(basketArr));
+          }
+        } else {
+          evt.target.classList.remove('js-remove');
+          evt.target.classList.add('js-add');
+
+          document.querySelector('.book-modal-buy').style.display = 'none';
+
+          if (inStorage) {
+            basketArr.splice(obj, 1);
+            localStorage.setItem('books', JSON.stringify(basketArr));
+          }
+        }
+      }
+    })
+    .catch(err => console.log(err));
+}
+// localStorage.clear();
